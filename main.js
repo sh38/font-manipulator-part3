ee673f269a3f48612fd2cbd88a47eca093715860
sh38 <83451771+sh38@users.noreply.github.com>
2022-12-27 18:40:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+leftWristX = 0;
+rightWristX = 0;
+difference = 0;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    
+    canvas = createCanvas(550, 450);
+    canvas.position(560,100);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    }
+    
+    function draw() {
+        background('#969A97');
+        fill('#F90093');
+        textSize(difference);
+        text('shivesh', 50,400);
+    }
+    
+    function modelLoaded() {
+        console.log('PoseNet Is Initialized!');
+    }
+    
+    function gotPoses(results)
+    {
+        if(results.length > 0)
+        {
+            console.log(results);
+            leftWristX = results[0].pose.leftWrist.x;
+            rightWristX = results[0].pose.rightWrist.x;
+            difference = floor(leftWristX - rightWristX)
+            console.log("leftWristX = " + leftWristX +" rigthWristX = " + rightWristX);
+        }
+    }
